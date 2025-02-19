314453d768dcdfb4ce8b1c0fdadd3d1d322bdba7
divyang-chhantbar <divyangchhantbar@gmail.com>
2025-02-19 17:53:58 +0530

    Add database connection utility, SMTP transporter, and utility functions

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?: number;
+}
+
+const connection: ConnectionObject = {};
+
+// the above thing we have done for the first time so let me explain what we did in detail : 
+// we have created a connection object of type ConnectionObject which is an interface that has a property isConnected of type number. This property will be used to check if the connection is already established or not. If it is established, then we will not establish the connection again.
+
+// Now let's create a function that will connect to the database.
+export async function dbConnect(): Promise<void> {
+    if(connection.isConnected){
+        console.log("Already connected to the database");
+    }
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "" ,{})
+        console.log("DB details :",db);
+        console.log("db.connections details :",db.connections);
+        connection.isConnected = db.connections[0].readyState;
+        console.log("DB connection established successfully");
+    } catch (error) {
+        console.log("DB connection failed",error);
+        process.exit(1);
+    }
+}
---
+++ src/lib/smtpTransporter.ts
@@ -0,0 +1,10 @@
+import nodemailer from 'nodemailer';
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER, // Your Gmail address
+    pass: process.env.GMAIL_PASS, // Your App Password from Gmail
+  },
+});
+
+export default transporter;
