a54f59dc6cba694b6d71318c7492d46b4aa3634b
divyang-chhantbar <divyangchhantbar@gmail.com>
2025-04-01 17:47:00 +0530

    Add DashboardPage component to render the Dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Dashboard from "@/components/Dashboard";
+
+export default function DashboardPage() {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
+            <Dashboard />
+        </div>
+    )
+}
