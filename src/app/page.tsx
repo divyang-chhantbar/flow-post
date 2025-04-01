ec65ecd0c8d6acf3cfda34073dc78090badd4225
divyang-chhantbar <divyangchhantbar@gmail.com>
2025-04-01 17:46:53 +0530

    Add main app page component with sections for Home, Features, How It Works, Testimonials, and Call to Action

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import Home from "@/components/HeroSection";
+import FeaturesSection from "@/components/FeatureSection";
+import HowItWorks from "@/components/HowItWorks";
+import TestimonialsGrid from "@/components/Testimonial";
+import CallToAction from "@/components/CTASection";
+
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 dark:bg-gray-900">
+      <Home />
+      <FeaturesSection />
+      <HowItWorks />
+      <TestimonialsGrid />
+      <CallToAction />
+    </div>
+  );
+}
