f6a8ffbb1f5908a235174ad9be6d8401e94fa01b
divyang-chhantbar <divyangchhantbar@gmail.com>
2025-04-25 18:14:54 +0530

    Remove unique constraint from name field and ensure uniqueness through a compound index on userId and name.

--- src/models/category.model.ts
+++ src/models/category.model.ts
@@ -15,13 +15,17 @@ const categorySchema : Schema = new Schema({
     name : {
         type : String,
         required : [true, 'Name is required'],
-        unique : [true, 'Name must be unique']
     },
     description : {
         type : String,
         required : [true, 'Description is required']
     }
 },{timestamps : true});
+
+categorySchema.index({userId : 1, name : 1}, {unique : true});
+// here we are creating a compound index on userId and name fields to ensure that the combination of userId and name is unique across the collection.
+// This means that a user cannot have two categories with the same name.
+// sometimes we need scoped uniqueness and not global uniqueness.
 
 const CategoryModel = (mongoose.models.Category as mongoose.Model<Category>) || mongoose.model<Category>('Category', categorySchema);
 
